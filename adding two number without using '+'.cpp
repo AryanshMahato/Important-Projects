3416f35ad3d22e78c1d593eb12a47945bed44aa1
Aryansh <aryanshmahato@gmail.com>
2019-05-19 11:19:14 +0530

    making a file short

--- adding two number without using '+'.cpp
+++ adding two number without using '+'.cpp
@@ -1,41 +1,19 @@
 #include<iostream>
 using namespace std;
 
-void arrange(int &n1, int &n2);
-
 
 int main()
 {
-    int num1, num2, num3;
+    int a, b;
+    cin>>a>>b;
 
-    cout<<"Enter first number: ";       cin>>num1;
-    cout<<"Enter second number: ";       cin>>num2;
-
-    arrange(num1, num2);
-
-    while(num1>0){
-        --num1;
-        ++num2;
+    while(b!=0)
+    {
+        ++a;
+        --b;
     }
 
-    cout<<endl<<"Answer is: "<<num2<<endl;
+    cout<<a<<endl<<endl;
 
-    cout<<endl<<endl;       //for beautification
-    return 0;
-}
-
-
-void arrange(int &n1, int &n2){
-    if(n1>n2){
-        n1=n1;
-    }
-    else if(n2>n1){
-        int temp;
-        temp=n2;
-        n2=n1;
-        n1=temp;
-    }
-    else{
-        cout<<"Same Number! But no problem"<<endl;
-    }
+	return 0;
 }
